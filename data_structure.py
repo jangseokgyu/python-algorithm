5e57d6571427fc2a3b2131fc997bf506b949a57b
jangseokgyu <43026098+jangseokgyu@users.noreply.github.com>
2019-01-09 08:10:14 +0900

    Update data_structure.py

--- data_structure.py
+++ data_structure.py
@@ -1,5 +1,18 @@
-age =22
-if age>20:
-  print("fuck")
-elif age > 44:
-  print("you")
+a = float(input("정수 1 입력하시오"))
+b = float(input("정수 2 입력하시오"))
+c = input("연산자 입력하시오")
+
+if c == '*' :
+    d = a*b
+elif c == '+':
+    d = a+b
+elif c == '-':
+    d = a-b
+elif c == '/':
+    d = a//b
+elif c == '**':
+    d = a**b
+else :
+    print("시벌로마 제대로 입력해라")
+
+print("결과는 : ", d , " 입니다")
